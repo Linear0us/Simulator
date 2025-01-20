e9d151ff13c93fcdee04d06301f17458d1211db1
Linear0us <xfkr2303@gmail.com>
2025-01-20 23:50:47 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simulator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Player player = null;
+        Random random = new Random(); //随机事件
+        
+        private void Update()   //更新屏幕上的数据
+        {
+            Days.Text = "Day " + player.Days.ToString();
+            Foods.Text = "Foods: " + player.Food.ToString();
+        }
+
+        private void InfoUpdate(string s)   //更新文本框的数据
+        {
+            Info.Text += s;
+            Info.ScrollToEnd();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();  //窗口初始化
+
+            player = new Player();  //实例化玩家
+
+            Update(); //刷新内容
+        }
+
+        private async void NextDay_Click(object sender, RoutedEventArgs e)
+        {
+            bool type = player.NextDay();   //进入下一天
+            if (!type)
+            {
+                InfoUpdate("\n由于缺乏食物, 你没能看到第二天的太阳...程序10s后关闭");
+
+                //禁用按钮
+                NextDay.IsEnabled = false;
+                Explore.IsEnabled = false;
+
+                await Task.Delay(10000); //异步关闭窗口
+                this.Close();
+            }
+
+            Explore.IsEnabled = true;
+
+            Update();
+        }
+
+        private void Explore_Click(object sender, RoutedEventArgs e)
+        {
+            int type = random.Next(1, 100000) % 3; //随机数
+            if (type == 0) //没找到
+            {
+                InfoUpdate("\n你什么也没找到...");
+            }
+            else if (type == 1) //找到
+            {
+                int food = random.Next(1, 5); 
+                InfoUpdate("\n你找到了" + food + "个食物");
+                player.GetFood(food); //同步更改
+            }
+            else if(type == 2) //战斗
+            {
+                InfoUpdate("\n进入战斗（未完成）");
+            }
+
+            Explore.IsEnabled = false; //一天只能探索一次
+
+            Update();
+            return;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    public class Player
+    {
+        private int days;       //存活天数
+        private int hitPoint;   //生命值
+        private int food;       //食物数量
+
+        public int Days
+        {
+            get { return days; }
+        }
+        
+        public int HitPoint
+        {
+            get { return hitPoint; }
+        }
+
+        public int Food
+        {
+            get { return food; }
+        }
+
+        public bool NextDay() //进入下一天
+        {
+            if (food > 0) { food--; } //食物不够扣10hp
+            else { hitPoint -= 10; }
+
+            if (HitPoint <= 0) //没血死亡
+            { 
+                return false;
+            }
+            else 
+            { 
+                days++;
+                return true;
+            }
+        }
+
+        public void GetFood(int quantity)
+        {
+            food += quantity;
+        }
+
+        public Player() 
+        { 
+            hitPoint = 100;
+            food = 10;
+            days = 1;
+        }
+    }
+}
